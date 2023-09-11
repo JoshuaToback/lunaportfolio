56421a61595785bf7d227f2d82c6ceced057a040
JoshuaToback <joshuatoback@gmail.com>
2023-09-11 13:45:59 -0400

    Added experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,11 +1,111 @@
-import React from 'react'
+import React from "react";
+
+import check from "../assets/checkmark.png";
 
 function Experience() {
   return (
-    <div>
-      <h1>Experience</h1>
+    <div className="experience">
+      <h1 className="title">Experience</h1>
+      <div className="experience-details-container">
+        <div className="about-containers">
+          <div className="details-container">
+            <h2 className="experience-sub-title">Graphic Design</h2>
+            <div className="article-container">
+              <article>
+                <img src={check} alt="checkmark" className="icon" />
+                <div>
+                  <h3>Skill </h3>
+                  <p>Skill Level</p>
+                </div>
+              </article>
+              <article>
+                <img src={check} alt="checkmark" className="icon" />
+                <div>
+                  <h3>Skill </h3>
+                  <p>Skill Level</p>
+                </div>
+              </article>
+              <article>
+                <img src={check} alt="checkmark" className="icon" />
+                <div>
+                  <h3>Skill </h3>
+                  <p>Skill Level</p>
+                </div>
+              </article>
+              <article>
+                <img src={check} alt="checkmark" className="icon" />
+                <div>
+                  <h3>Skill </h3>
+                  <p>Skill Level</p>
+                </div>
+              </article>
+              <article>
+                <img src={check} alt="checkmark" className="icon" />
+                <div>
+                  <h3>Skill </h3>
+                  <p>Skill Level</p>
+                </div>
+              </article>
+              <article>
+                <img src={check} alt="checkmark" className="icon" />
+                <div>
+                  <h3>Skill </h3>
+                  <p>Skill Level</p>
+                </div>
+              </article>
+            </div>
+          </div>
+          <div className="details-container">
+            <h2 className="experience-sub-title">Graphic Design</h2>
+            <div className="article-container">
+              <article>
+                <img src={check} alt="checkmark" className="icon" />
+                <div>
+                  <h3>Skill </h3>
+                  <p>Skill Level</p>
+                </div>
+              </article>
+              <article>
+                <img src={check} alt="checkmark" className="icon" />
+                <div>
+                  <h3>Skill </h3>
+                  <p>Skill Level</p>
+                </div>
+              </article>
+              <article>
+                <img src={check} alt="checkmark" className="icon" />
+                <div>
+                  <h3>Skill </h3>
+                  <p>Skill Level</p>
+                </div>
+              </article>
+              <article>
+                <img src={check} alt="checkmark" className="icon" />
+                <div>
+                  <h3>Skill </h3>
+                  <p>Skill Level</p>
+                </div>
+              </article>
+              <article>
+                <img src={check} alt="checkmark" className="icon" />
+                <div>
+                  <h3>Skill </h3>
+                  <p>Skill Level</p>
+                </div>
+              </article>
+              <article>
+                <img src={check} alt="checkmark" className="icon" />
+                <div>
+                  <h3>Skill </h3>
+                  <p>Skill Level</p>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
